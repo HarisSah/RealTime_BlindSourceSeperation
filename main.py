e695477955003640c609fcc653a04a2fa478e1bc
Haris Sahmanovic <151585577+HarisSah@users.noreply.github.com>
2023-12-17 15:32:25 -0500

    Updated Code 12/17
    Updated code with more functionality and was able to plot something.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from sklearn.decomposition import FastICA
 import pyaudio
 import wave
 
+# Wavefiles application
 class BSSApp:
     def __init__(self, root):
         self.root = root
@@ -14,13 +15,14 @@ class BSSApp:
         self.audio_file_1 = None
         self.audio_file_2 = None
         self.original_signal = None
-        self.result_signal_1 = None
         self.result_signal_2 = None
+        self.result_signal_3 = None
 
         # Create buttons
+        self.play_button_1 = tk.Button(root, text="PLAY ORIGINAL", command=lambda: self.original_audio())
         self.start_button = tk.Button(root, text="START", command=self.run_ica)
-        self.play_button_1 = tk.Button(root, text="PLAY AUDIO 1", command=lambda: self.play_audio(self.result_signal_1, duration=5))
-        self.play_button_2 = tk.Button(root, text="PLAY AUDIO 2", command=lambda: self.play_audio(self.result_signal_2, duration=5))
+        self.play_button_2 = tk.Button(root, text="PLAY AUDIO 1", command=lambda: self.play_audio(self.result_signal_2, duration=5))
+        self.play_button_3 = tk.Button(root, text="PLAY AUDIO 2", command=lambda: self.play_audio(self.result_signal_3, duration=5))
         self.plot_button = tk.Button(root, text="PLOT", command=self.plot_signals)
         self.quit_button = tk.Button(root, text="QUIT", command=root.destroy)
 
@@ -31,12 +33,21 @@ class BSSApp:
         self.start_button.pack(pady=10)
         self.play_button_1.pack(pady=10)
         self.play_button_2.pack(pady=10)
+        self.play_button_3.pack(pady=10)
         self.plot_button.pack(pady=10)
         self.quit_button.pack(pady=10)
         self.status_label.pack(pady=10)
 
         # Initialize PyAudio
         self.p = pyaudio.PyAudio()
+
+    def original_audio(self, duration=5):
+        stream = self.p.open(format=pyaudio.paInt16,
+                             channels=1,
+                             rate=48000,
+                             output=True)
+        stream.write(self.original_signal.tobytes())
+        self.root.after(int(duration * 1000), lambda: self.stop_audio(stream))
 
     def read_wav(self, file_path):
         wf = wave.open(file_path, 'rb')
@@ -52,8 +63,8 @@ class BSSApp:
         self.root.update()
 
         # Load mixed audio data
-        voice_1, fs_1 = self.read_wav("Audio_Files/mix_type_2_1.wav") 
-        voice_2, fs_2 = self.read_wav("Audio_Files/mix_type_2_2.wav") 
+        voice_1, fs_1 = self.read_wav("Audio_Files/mix_type_2_1.wav")
+        voice_2, fs_2 = self.read_wav("Audio_Files/mix_type_2_2.wav")
 
         # Reshape the files to have the same size
         m = min(len(voice_1), len(voice_2))
@@ -75,11 +86,17 @@ class BSSApp:
         self.write_wav("estimated_voice_2.wav", S_[:, 1], fs_2)
 
         # Update result signals for playback
-        self.result_signal_1 = S_[:, 0]
-        self.result_signal_2 = S_[:, 1]
+        self.result_signal_2 = S_[:, 0]
+        self.result_signal_3 = S_[:, 1]
+
+        # Load original mixed audio data (this is the part you were missing)
+        self.original_signal, _ = self.read_wav("Audio_Files/mix_type_2_1.wav")
 
         # Change status label to "READY"
         self.status_label.config(text="READY", fg="green")
+
+        # Call plot_signals to update the plot
+        self.plot_signals()
 
     def play_audio(self, data, duration):
         stream = self.p.open(format=pyaudio.paInt16,
@@ -100,22 +117,18 @@ class BSSApp:
         stream.stop_stream()
         stream.close()
 
-    def plot_signals(self): # the function implementation needs to be reworked
-        if self.original_signal is not None and self.result_signal_1 is not None and self.result_signal_2 is not None:
+    def plot_signals(self):
+        if self.original_signal is not None and self.result_signal_2 is not None:
             # Plot original and separated signals
             plt.figure(figsize=(12, 6))
 
-            plt.subplot(3, 1, 1)
+            plt.subplot(2, 1, 1)
             plt.plot(self.original_signal)
-            plt.title("Original Signals")
+            plt.title("Original Signal")
 
-            plt.subplot(3, 1, 2)
-            plt.plot(self.result_signal_1)
-            plt.title("Separated Signal 1")
-
-            plt.subplot(3, 1, 3)
+            plt.subplot(2, 1, 2)
             plt.plot(self.result_signal_2)
-            plt.title("Separated Signal 2")
+            plt.title("Separated Signal")
 
             plt.tight_layout()
             plt.show()
