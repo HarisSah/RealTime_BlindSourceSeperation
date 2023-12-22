b144cc43577e5935ff9e2006e5b472e9f3ff3ce3
Saksham Madan <43085346+Hawk453@users.noreply.github.com>
2023-12-21 19:40:22 -0500

    Add files via upload

--- STFT.m
+++ STFT.m
@@ -5,27 +5,36 @@ close all
 % STFT Plot of a Wave File
 
 % Specify the path to your wave file
-waveFilePath = 'estimated_voice_2_realtime.wav';
+waveFilePath = 'estimated_voice_2.wav';
 
 % Read the audio file
 [x, fs] = audioread(waveFilePath);
 
 % STFT parameters
 windowSize = 512;
-hopSize = 256;
+hopSize = 128;
 
 % Calculate the STFT
 [S, F, T] = spectrogram(x, windowSize, hopSize, windowSize, fs);
 
-% Plot the STFT
+% Plot the STFT with enhanced color contrast
 figure;
-surf(T, F, 10 * log10(abs(S)), 'EdgeColor', 'none');
+surf(T, F, 10 * log10(abs(S)),'EdgeColor','none');
 axis tight;
 view(0, 90);  % To view the plot from the top
 xlabel('Time (s)');
 ylabel('Frequency (Hz)');
 title('STFT of the Wave File');
 colorbar;
+
+
+% Choose a colormap with high contrast
+colormap(jet);
+
+% Adjust the color axis scaling for better contrast
+clim([max(10 * log10(abs(S(:)))) - 50, max(10 * log10(abs(S(:))))]);
+
+
 
 % Display the original waveform
 figure;
